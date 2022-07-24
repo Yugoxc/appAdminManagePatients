0377b0ae04e3203c313094371179962fbfae0312
Yugoxc <dee1222002@gmail.com>
2022-07-24 00:13:33 -0400

    Agregar archivos de proyecto.

---
+++ appAdminManage/appAdminManage/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using appAdminManage.ViewModels;
+using appAdminManage.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace appAdminManage
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ appAdminManage/appAdminManage/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using appAdminManage.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace appAdminManage.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
